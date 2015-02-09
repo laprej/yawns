1afb7568e2ff9c22d3d2bc2d4a649eae5a37435e
Justin M. LaPre <justin@lapre.com>
2015-02-09 16:21:16 -0500

    Migrated gvt_step{1,2} into cyawns.  Not working yet.

--- omnetpp-4.4.1/src/sim/parsim/cyawns.cc
+++ omnetpp-4.4.1/src/sim/parsim/cyawns.cc
@@ -186,6 +186,130 @@ void cYAWNS::processReceivedBuffer(cCommBuffer *buffer, int tag, int sourceProcI
     buffer->assertBufferEmpty();
 }
 
+enum gvt_status { TW_GVT_NORMAL, TW_GVT_COMPUTE };
+
+static gvt_status local_gvt_status = TW_GVT_NORMAL;
+
+static unsigned gvt_cnt = 0;
+static unsigned g_tw_gvt_interval = 16;
+
+void
+tw_gvt_step1(void)
+{
+    if(local_gvt_status == TW_GVT_COMPUTE ||
+       ++gvt_cnt < g_tw_gvt_interval)
+        return;
+
+    local_gvt_status = TW_GVT_COMPUTE;
+}
+
+void
+tw_gvt_step2(void)
+{
+    long long local_white = 0;
+    long long total_white = 0;
+
+    SimTime pq_min = SimTime::getMaxTime();
+    SimTime net_min = SimTime::getMaxTime();
+
+    SimTime lvt;
+    SimTime gvt;
+
+    simtime_t_cref start = simTime();
+
+    if(local_gvt_status != TW_GVT_COMPUTE)
+        return;
+
+    while(1)
+    {
+        tw_net_read(me);
+
+        // send message counts to create consistent cut
+        local_white = me->s_nwhite_sent - me->s_nwhite_recv;
+        all_reduce_cnt++;
+        if(MPI_Allreduce(
+                         &local_white,
+                         &total_white,
+                         1,
+                         MPI_LONG_LONG,
+                         MPI_SUM,
+                         MPI_COMM_WORLD) != MPI_SUCCESS)
+            tw_error(TW_LOC, "MPI_Allreduce for GVT failed");
+
+        if(total_white == 0)
+            break;
+    }
+
+    pq_min = tw_pq_minimum(me->pq);
+    net_min = tw_net_minimum(me);
+
+    lvt = me->trans_msg_ts;
+    if(lvt > pq_min)
+        lvt = pq_min;
+    if(lvt > net_min)
+        lvt = net_min;
+
+    all_reduce_cnt++;
+    if(MPI_Allreduce(
+                     &lvt,
+                     &gvt,
+                     1,
+                     MPI_DOUBLE,
+                     MPI_MIN,
+                     MPI_COMM_WORLD) != MPI_SUCCESS)
+        tw_error(TW_LOC, "MPI_Allreduce for GVT failed");
+
+    gvt = ROSS_MIN(gvt, me->GVT_prev);
+
+    if(gvt != me->GVT_prev)
+    {
+        g_tw_gvt_no_change = 0;
+    } else
+    {
+        g_tw_gvt_no_change++;
+        if (g_tw_gvt_no_change >= g_tw_gvt_max_no_change) {
+            tw_error(
+                     TW_LOC,
+                     "GVT computed %d times in a row"
+                     " without changing: GVT = %14.14lf, PREV %14.14lf"
+                     " -- GLOBAL SYNCH -- out of memory!",
+                     g_tw_gvt_no_change, gvt, me->GVT_prev);
+        }
+    }
+
+    if (me->GVT > gvt)
+    {
+        tw_error(TW_LOC, "PE %u GVT decreased %g -> %g",
+                 me->id, me->GVT, gvt);
+    }
+
+    if (gvt / g_tw_ts_end > percent_complete && (g_tw_mynode == g_tw_masternode)) {
+        gvt_print(gvt);
+    }
+
+    me->s_nwhite_sent = 0;
+    me->s_nwhite_recv = 0;
+    me->trans_msg_ts = DBL_MAX;
+    me->GVT_prev = DBL_MAX; // me->GVT;
+    me->GVT = gvt;
+    me->gvt_status = TW_GVT_NORMAL;
+
+    gvt_cnt = 0;
+
+    // update GVT timing stats
+    me->stats.s_gvt += tw_clock_read() - start;
+
+    // only FC if OPTIMISTIC
+    if( g_tw_synchronization_protocol == OPTIMISTIC )
+    {
+        start = tw_clock_read();
+        tw_pe_fossil_collect(me);
+        me->stats.s_fossil_collect += tw_clock_read() - start;
+    }
+
+    g_tw_gvt_done++;
+}
+
 cMessage *cYAWNS::getNextEvent()
 {
     // our EIT and resendEOT messages are always scheduled, so the FES can
